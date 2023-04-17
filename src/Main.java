fe7cacbd6e90f44ea3d18c57d649a4a83d32f242
yafist <127660608+yafist@users.noreply.github.com>
2023-04-17 20:55:00 +0400

    Delete Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        LogTest Ma = new LogTest();
-        boolean TrueLogin = Ma.isLoginValid("yaf1st");
-        if (TrueLogin) System.out.println("Логин подходит!");
-        else System.out.println("Логин не подходит!");
-    }
-}
